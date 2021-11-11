5069d41d4aedebb42b0e02022240ef0f2aeb2741
Cerkes Mirza <almas1610@gmail.com>
2021-11-11 07:02:40 +0600

    golang new project

---
+++ hello.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func main() {
+
+	fmt.Println("Go!")
+	s := strconv.Itoa(45)
+	fmt.Println(s)
+
+	// func ToString(x int) string {
+	// 	s := strconv.Itoa(x)
+	// 	return s
+	// 	fmt.Println()
+	// }
+	// ToString(45)
+}
